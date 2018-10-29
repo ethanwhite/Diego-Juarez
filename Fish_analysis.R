bdb33a1e20662f9d2d73842ca74ede5ddeb10f2c
Diego Juárez <diegojuarez@MacBook-Pro-de-Diego-Juarez.local>
2018-10-29 14:55:18 -0400

    rename the data set

--- Fish_analysis.R
+++ Fish_analysis.R
@@ -1,8 +1,22 @@
 # in this proyect we are going to analice fish size growth rate data 
 
-fish_data <- read_csv("Gaeta_etal_CLC_data.csv")
+fish_data <- read_csv("Gaeta_etal_CLC_data_1.csv")
 
 library(dplyr)
 fish_data_cat = fish_data %>% 
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+# remove the fish with a scalelength of less than 1 mm 
+
+fish_data_cat<-filter(fish_data_cat, scalelength > 1 )
+
+
+
+
+
+
+
+
+
+
 
